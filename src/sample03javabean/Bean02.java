82ee2410e21309484f09f6da21a215cd09103f99
Jsiyoon <jjc9090@naver.com>
2021-11-05 14:43:52 +0900

    JavaBean-javafile

---
+++ src/sample03javabean/Bean02.java
@@ -0,0 +1,23 @@
+package sample03javabean;
+
+public class Bean02 {
+	private String name;
+	private int age;
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/sample03javabean/Bean04.java
@@ -0,0 +1,27 @@
+package sample03javabean;
+
+public class Bean04 {
+	private String nickname;
+	private int age;
+	private boolean vip;
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isVip() {
+		return vip;
+	}
+	public void setVip(boolean vip) {
+		this.vip = vip;
+	}
+	
+	
+}

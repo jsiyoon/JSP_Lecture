0a89a97954cb13f33a6b7622c3b350df61044e7d
Jsiyoon <jjc9090@naver.com>
2021-11-21 15:39:21 +0900

    /WEB-INF/web.xml에 fiter경로 설정해 filterex3의 모든 경로에 접근할 수 있도록 함.

---
+++ src/filter03/f08filter.java
@@ -0,0 +1,50 @@
+package filter03;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class f08filter
+ */
+//@WebFilter("/f08filter")
+public class f08filter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public f08filter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		System.out.println("8번 필터 실행");
+		// pass the request along the filter chain
+		chain.doFilter(request, response); //다음 필터나 서블릿 등이 실행되기 위해 존재해야함.
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}

b4c1a8b073b723b609e8934bbfe72aec180b8f49
Jsiyoon <jjc9090@naver.com>
2021-11-22 15:56:46 +0900

    javaBean&list사용해 데이터 베이스의 여러가지 값 불러와 출력하기

---
+++ src/jdbc02/bean/Customer.java
@@ -0,0 +1,36 @@
+package jdbc02.bean;
+
+public class Customer {
+	private String customerName;
+	private String contactName;
+	private String address;
+	private String city;
+	
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+ 
+}

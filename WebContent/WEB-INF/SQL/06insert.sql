31d32866e716c21d858bd85fde5934e6bae1cac2
Jsiyoon <jjc9090@naver.com>
2021-11-24 15:51:05 +0900

    DB INSERT

---
+++ WebContent/WEB-INF/SQL/06insert.sql
@@ -0,0 +1,28 @@
+USE test;
+
+SELECT * FROM Customers;
+
+SELECT * FROM Customers ORDER BY CustomerID DESC;
+
+INSERT INTO Customers VALUES(92, 'Kim', 'Kim', 'GangNam', 'Seoul', '88888', 'korea');
+
+INSERT INTO Customers (CustomerID, CustomerName) VALUES(93, 'Lee');
+
+INSERT INTO Customers (CustomerID, CustomerName) VALUES (94, 'choi');
+
+INSERT INTO Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES('95', 'Park', 'JiSung', 'yeoksam', 'Seoul', '77777', 'Korea');
+
+INSERT INTO Customers VALUES('96', 'Jimin', 'Apartment', 'Seocho', 'Seoul', '12345', 'Korea');
+
+INSERT INTO Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('97', 'bts', 'CentralPark', 'Manhattan', 'NY', '54321', 'USA');
+
+INSERT INTO Customers(CustomerName) VALUES('blockb');
+
+INSERT INTO Customers(CustomerName, Address) VALUES('Steven','Apple');
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('bts', 'CentralPark', 'Manhattan', 'NY', '54321', 'USA');
+
+SELECT * FROM Suppliers ORDER BY SupplierID DESC;

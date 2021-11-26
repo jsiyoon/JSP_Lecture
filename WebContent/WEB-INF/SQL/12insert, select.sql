51fefbdeb862c8f8986dc183183b4a74124be8ad
Jsiyoon <jjc9090@naver.com>
2021-11-26 12:29:35 +0900

    DB - INSERT, SELECT (/src/jdbc08/)

---
+++ WebContent/WEB-INF/SQL/12insert, select.sql
@@ -0,0 +1,20 @@
+USE test;
+
+CREATE TABLE mytable10(
+	col1 INT,
+    col2 double,
+    col3 DEC(10, 3),
+    col4 CHAR(3),
+    col5 VARCHAR(255),
+    col6 DATE,
+    col7 DATETIME
+);
+
+SELECT * FROM mytable10;
+
+-- JDBC37Servlet, JDBC38Servlet
+CREATE TABLE mytable11(
+	name VARCHAR(255),
+    age INT,
+    birth DATE
+);
